ce97b1de04404926b9ddbe084e5d0d6af9dd78a4
kevinphillips <inquiries@kevinphillips.email>
2021-01-03 02:49:48 -0500

    ignore dataclass with python v3.5

--- missionchurch/missionChurch/items.py
+++ missionchurch/missionChurch/items.py
@@ -6,8 +6,8 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-from dataclasses import dataclass, field
-from typing import Optional
+# from dataclasses import dataclass, field
+# from typing import Optional
 
 
 class HighlightItem(scrapy.Item):
@@ -17,14 +17,13 @@ class HighlightItem(scrapy.Item):
     pass
 
 
-# not available in Python v3.5 (running on server)
 # @dataclass
 # class HighlightDC(scrapy.Item):
 #     highlightId: Optional[str] = field(default=None)
 #     title: Optional[str] = field(default=None)
 #     caption: Optional[str] = field(default=None)
 
-# Not used, to demonstrate Python multi class extension below
+
 class Zoom(HighlightItem):
     followUrl = scrapy.Field()
     meetingID = scrapy.Field()
